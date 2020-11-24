a663277703a187399f28043fe258d1fdc21ef491
Kunwar <kunwarv231@gmail.com>
2020-11-24 12:11:13 +0530

    code update

--- Pods/Target Support Files/Pods-OCRSample-OCRSampleUITests/Pods-OCRSample-OCRSampleUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_OCRSample_OCRSampleUITests : NSObject
-@end
-@implementation PodsDummy_Pods_OCRSample_OCRSampleUITests
-@end
--- Pods/Target Support Files/Pods-OCRSampleTests/Pods-OCRSampleTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_OCRSampleTests : NSObject
-@end
-@implementation PodsDummy_Pods_OCRSampleTests
-@end
